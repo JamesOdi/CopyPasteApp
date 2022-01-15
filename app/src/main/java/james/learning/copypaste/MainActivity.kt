2dde0ef49308ffdc31835a748c1f9df5b36659d7
James Odike <Jamesodike26@gmail.com>
2022-01-15 09:58:49 +0100

    Add paste button click function

--- app/src/main/java/james/learning/copypaste/MainActivity.kt
+++ app/src/main/java/james/learning/copypaste/MainActivity.kt
@@ -1,6 +1,7 @@
 package james.learning.copypaste
 
 import android.content.ClipData
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
 import android.content.ClipboardManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity() {
 
         val randomTextField = findViewById<EditText>(R.id.randomText)
         val copyButton = findViewById<Button>(R.id.copyButton)
+        val pasteButton = findViewById<Button>(R.id.pasteButton)
+        val inputEditText = findViewById<EditText>(R.id.plainEditText)
+        
         randomTextField.setText(getRandomText())
 
         val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
@@ -23,7 +27,18 @@ class MainActivity : AppCompatActivity() {
             val randomText = randomTextField.text.toString()
             val clip = ClipData.newPlainText("label",randomText)
             clipboard.setPrimaryClip(clip)
-            Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Copied to clipboard!", Toast.LENGTH_SHORT).show()
+        }
+        
+        pasteButton.setOnClickListener {
+            if (!clipboard.hasPrimaryClip()) {
+                Toast.makeText(this, "Nothing to paste!", Toast.LENGTH_SHORT).show()
+            } else if (!(clipboard.primaryClipDescription?.hasMimeType(MIMETYPE_TEXT_PLAIN))!!) {
+                Toast.makeText(this, "Object cannot be pasted here!", Toast.LENGTH_SHORT).show()
+            } else {
+                val clipItem = clipboard.primaryClip?.getItemAt(0)!!
+                inputEditText.setText(clipItem.text.toString())
+            }
         }
     }
 
